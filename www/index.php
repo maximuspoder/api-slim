1c29e06e9f0097d910709fb301211910eb9de17a
= <developer.fernando@gmail.com>
2020-05-29 16:33:24 -0300

    .

--- www/index.php
+++ www/index.php
@@ -7,7 +7,8 @@ $app = new \Slim\App();
  * Simulate request into phoenix to get my creative project
  */
 $app->get('/phoenix/mine/project', function ($request, $response, $args) {
-
+    $active = true;
+    $value = 'Magnet M 5.6x5.6 cm';
     $id = $request->getParam('id') ? $request->getParam('id') : null;
     if (is_null($id)) {
         return $response
@@ -17,12 +18,18 @@ $app->get('/phoenix/mine/project', function ($request, $response, $args) {
     
     $stock = $request->getParam('stock') ? $request->getParam('stock') : 100;
     $price = $request->getParam('price') ? $request->getParam('price') : 9.95;
+    if ($id == '000C-L') {
+        $stock = 0;
+        $active = false;
+        $value = 'Magnet L 7.5x7.5 cm';
+    }
+        
     $data = [
         'id' => $id,
         'stock' => intval($stock),
         'price' => floatval($price),
-        'attributes' => ['type' => 'taille', 'value' => 'Magnet M 5.6x5.6 cm'],
-        'active' => true
+        'attributes' => ['type' => 'taille', 'value' => $value],
+        'active' => $active
     ];
     return $response
                 ->withHeader('Content-type', 'application/json')
