aa2110f62afef1a9a5d15645193cc6ed741d0c39
= <developer.fernando@gmail.com>
2020-05-29 16:28:48 -0300

    changed params

--- www/index.php
+++ www/index.php
@@ -6,18 +6,28 @@ $app = new \Slim\App();
 /**
  * Simulate request into phoenix to get my creative project
  */
-$app->get('/phoenix/mine/projects', function ($request, $response, $args) {
-    $id = $request->getParam('id') ? $request->getParam('id') : 100;
+$app->get('/phoenix/mine/project', function ($request, $response, $args) {
+
+    $id = $request->getParam('id') ? $request->getParam('id') : null;
+    if (is_null($id)) {
+        return $response
+                ->withHeader('Content-type', 'application/json')
+                ->withJson(["data" => []]);
+    }
+    
     $stock = $request->getParam('stock') ? $request->getParam('stock') : 100;
     $price = $request->getParam('price') ? $request->getParam('price') : 9.95;
     $data = [
-        'creative_project_id' => $id,
+        'id' => $id,
         'stock' => intval($stock),
-        'price' => floatval($price)
+        'price' => floatval($price),
+        'attributes' => ['type' => 'taille', 'value' => 'Magnet M 5.6x5.6 cm'],
+        'active' => true
     ];
     return $response
                 ->withHeader('Content-type', 'application/json')
-                ->withJson($data);
+                ->withJson(["data" => $data]);
 });
+
 
 $app->run();
